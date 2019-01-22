c4811f587c0516280df427f4225e04ca8da4b8f2
Namasta <fcchoong@gmail.com>
2019-01-22 21:05:24 +0800

    start

---
+++ index.js
@@ -0,0 +1,48 @@
+// See https://github.com/dialogflow/dialogflow-fulfillment-nodejs
+// for Dialogflow fulfillment library docs, samples, and to report issues
+'use strict';
+
+
+const express = require('express') ;
+const bodyParser = require('body-parser');
+const functions = require('firebase-functions');
+const {WebhookClient} = require('dialogflow-fulfillment');
+const {Card, Suggestion} = require('dialogflow-fulfillment');
+
+const app = dialogflow();
+ 
+process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+ 
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  const agent = new WebhookClient({ request, response });
+  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+ 
+  function welcome(agent) {
+    agent.add(`Welcome to my agent!`);
+  }
+ 
+  function fallback(agent) {
+    agent.add(`I didn't understand`);
+    agent.add(`I'm sorry, can you try again?`);
+}
+
+  
+
+  // Run the proper function handler based on the matched Dialogflow intent name
+  let intentMap = new Map();
+  intentMap.set('Default Welcome Intent', welcome);
+  intentMap.set('Default Fallback Intent', fallback);
+  // intentMap.set('your intent name here', yourFunctionHandler);
+  // intentMap.set('your intent name here', googleAssistantHandler);
+  agent.handleRequest(intentMap);
+});
+
+const exapp=express().use(bodyParser.json);
+exapp.post('/fulfillment',app.dialogflowFirebaseFulfillment());
+
+var listener = app.listen(4000, process.env.IP, function () {
+    //var listener = app.listen(process.env.PORT, process.env.IP, function () {
+        console.log("server has started");
+        console.log('Listening on port ' + listener.address().port);
+    });
