b2634866b0d41e0a75fb910b756b59925f52ba96
Namasta <fcchoong@gmail.com>
2019-01-23 17:08:20 +0800

    .

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ const exapp = express().use(bodyParser.json);
 exapp.post('/fulfillment', app.dialogflowFirebaseFulfillment);
 
 //var listener = app.listen(4000, process.env.IP, function () {
-    var listener = app.listen(process.env.PORT, process.env.IP, function () {
+    var listener = exapp.listen(process.env.PORT, process.env.IP, function () {
     console.log("server has started");
     console.log('Listening on port ' + listener.address().port);
 });
