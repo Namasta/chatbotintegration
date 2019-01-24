8ed8410a829e31061457b34046d7c0f26741ef07
Namasta <fcchoong@gmail.com>
2019-01-24 15:52:07 +0800

    .

--- try.js
+++ try.js
@@ -1,5 +1,23 @@
 //This section list the required libraries
-var express = require("express");
+'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+const functions = require('firebase-functions');
+const { WebhookClient } = require('dialogflow-fulfillment');
+const { Card, Suggestion } = require('dialogflow-fulfillment');
+var admin = require("firebase-admin");
+
+const {
+    dialogflow,
+    Image,
+    Table,
+    Carousel,
+  } = require('actions-on-google');
+  
+  var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
+  var VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
+  var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
+
 //Create an instance of express server
 var app = express();
 
